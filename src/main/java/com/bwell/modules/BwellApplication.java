50f5e7ebdf9b3bfe3b0e1601208870e045c53232
Dariusz Okonski <darek200180@gmail.com>
2021-10-20 16:40:13 +0100

    check if github working

--- src/main/java/com/bwell/modules/BwellApplication.java
+++ src/main/java/com/bwell/modules/BwellApplication.java
@@ -9,6 +9,7 @@ public class BwellApplication {
     public static void main(String[] args) {
         SpringApplication.run(BwellApplication.class, args);
         System.out.println("STARTING MODIFIE 11111111D");
+        System.out.println("check github");
     }
 
 }
