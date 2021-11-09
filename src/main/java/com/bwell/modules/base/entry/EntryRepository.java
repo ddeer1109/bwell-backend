da57b76239220c7b3b06c7df54dbb1503143d4cb
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-11-09 02:57:31 +0100

    fixing

--- src/main/java/com/bwell/modules/base/entry/EntryRepository.java
+++ src/main/java/com/bwell/modules/base/entry/EntryRepository.java
@@ -1,9 +1,10 @@
 package com.bwell.modules.base.entry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
     List<Entry> findAllByModuleEquals(String module);
 }
--- src/main/java/com/bwell/modules/user/data/repository/UserRepository.java
+++ src/main/java/com/bwell/modules/user/data/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.bwell.modules.user.data.repository;
 
 import com.bwell.modules.user.data.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByCredentialsProviderId(String id);
 }
