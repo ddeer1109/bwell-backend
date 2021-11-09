608c4fa690a152b1e5570cc25e6ba8569403be2c
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-11-09 17:41:27 +0100

    give config for create drop database

--- src/main/java/com/bwell/modules/BwellApplication.java
+++ src/main/java/com/bwell/modules/BwellApplication.java
@@ -2,6 +2,7 @@ package com.bwell.modules;
 
 import com.bwell.modules.base.BaseService;
 import com.bwell.modules.base.entry.Entry;
+import com.bwell.modules.configuration.DatabasePopulationConfig;
 import com.bwell.modules.user.data.model.User;
 import com.bwell.modules.user.data.service.UserService;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -25,13 +26,15 @@ public class BwellApplication {
     }
 
     @Bean
-    CommandLineRunner runner(BaseService baseService, UserService userService) {
+    CommandLineRunner runner(BaseService baseService, UserService userService, DatabasePopulationConfig config) {
+        if (!config.getDecision().equals("true"))
+            return null;
         return args -> {
             // read json and write to db
             log.info("!!!! ==>>>!!!! ==>>>!!!! ==>>>!!!! ==>>>!!!! ==>>>!!!! ==>>>");
-//            User defaultUser = userService.saveUser(UserService.createEmptyUser());
-//            defaultUser.getDietPlan().setUser(defaultUser);
-//            User.defaultUserId = defaultUser.getId();
+            User defaultUser = userService.saveUser(UserService.createEmptyUser());
+            defaultUser.getDietPlan().setUser(defaultUser);
+            User.defaultUserId = defaultUser.getId();
             ObjectMapper mapper = new ObjectMapper();
             TypeReference<List<Entry>> typeReference = new TypeReference<List<Entry>>(){};
             InputStream inputStream = TypeReference.class.getResourceAsStream("/json/allEntries.json");
